25706fa4ac30297ff48b17c46c4cba2a6eaef6fe
Azathoth <memnarch@seznam.cz>
2015-10-20 23:41:23 +0200

    přidány nějaké komentáře

--- Petrlik-sources/get_map_grid.m
+++ Petrlik-sources/get_map_grid.m
@@ -6,10 +6,14 @@ global params
 
 cost_neighbor = 10;
 cost_diagonal = 5;
+% pravdìpodobnì cena, tato promìnná se nikde v kódu nejspíše dále nevyužije
+% FUCK YOU AND YOUR NOT REVISED? NOT COMMENTED AND NOT REFACTORED CODE!!!
 cst = zeros(params.world_dimensions(2,1)/params.cell_size, ...
     params.world_dimensions(2,1)/params.cell_size);
 
 if params.algorithm==3
+    % nechápu, proè, zapomenutá inicializace, algoritmus PSO pobìží o
+    % trochu déle než by mìl se správnou inicializací
     for k=1:params.world_dimensions(2,1)/params.cell_size+1
         for m=1:params.world_dimensions(1,1)/params.cell_size+1
             map_grid(k,m) = contains_obj([(m-1)*params.cell_size;(k-1)*params.cell_size], ...
@@ -26,6 +30,8 @@ else
         end
     end
     
+    % myslím, že toto je umìlé zvìtšení pøekážek o 1 vrstvu, a try catche
+    % jsou kvùli okrajùm mapy
     for k=1:params.world_dimensions(2,1)/params.cell_size
         for m=1:params.world_dimensions(1,1)/params.cell_size
             try
